fd8b22efe0b1e2bd87051fdc5b17dcbf62630f66
Akshay-lnxct <akshaybhanderi0123@gmail.com>
2023-03-21 22:49:28 +0530

    space add

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
             <Route path="/home" element={<Home />} />
             {/* <Route exact path="/" element={<Home />} /> */}
             {/* <Route exact path="*" element={<Home />} /> */}
+          
           </Routes>
         </Router>
       ) : (
